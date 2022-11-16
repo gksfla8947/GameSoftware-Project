ac3ea1bd3b475c5e7b470b16615dc569dd95cdfb
Min <26513158@naver.com>
2022-11-16 15:19:58 +0900

    Copied from Main Branches& minimap+ simple cylindarmap
    메인 브렌치 내용들 저장+ 오른쪽 아래 미니맵 생성, 일단은 원형의 실린더 형태를 이용한 맵 제작.

--- 04.Scripts/Moveable.cs
+++ 04.Scripts/Moveable.cs
@@ -11,7 +11,8 @@ public class Moveable : MonoBehaviour
     public Camera camera;
     public GameObject mousePoint;
     NavMeshAgent agent;
-    
+
+    float distance = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +26,8 @@ public class Moveable : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, distance, Input.mousePosition.z);
+
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit raycasthit;
             if(Physics.Raycast(ray,out raycasthit))
